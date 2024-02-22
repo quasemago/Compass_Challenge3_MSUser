e224c5c84a103f03832dee78a6a1c05a2adf6a84
quasemago <brunoronningfn@gmail.com>
2024-02-21 22:59:16 -0400

    feat: Adicionada documentação do Spring Docs (Swagger) aos controllers.

--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/LoginController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/LoginController.java
@@ -2,7 +2,13 @@ package com.compassuol.sp.challenge.msuser.web.controller;
 
 import com.compassuol.sp.challenge.msuser.security.jwt.dto.JwtTokenDTO;
 import com.compassuol.sp.challenge.msuser.security.jwt.service.JwtUserLoginService;
+import com.compassuol.sp.challenge.msuser.web.dto.ErrorMessageDTO;
 import com.compassuol.sp.challenge.msuser.web.dto.UserLoginRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +17,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Login", description = "API Login - Tem a responsabilidade de autenticar o login do usuário na API.")
 @RestController
 @RequestMapping("/v1/login")
 @RequiredArgsConstructor
 public class LoginController {
     private final JwtUserLoginService service;
 
+    @Operation(summary = "Autenticar na API", description = "Recurso para autenticação na API.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Autenticação realizada com sucesso. Retorno de um Bearer Token.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = JwtTokenDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Recurso não processado devido a requisição inválida ou credenciais inválidas.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<JwtTokenDTO> login(@Valid @RequestBody UserLoginRequestDTO request) {
         return ResponseEntity.ok(service.login(request));
--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
@@ -1,29 +1,67 @@
 package com.compassuol.sp.challenge.msuser.web.controller;
 
 import com.compassuol.sp.challenge.msuser.domain.service.UserService;
-import com.compassuol.sp.challenge.msuser.web.dto.UserPasswordRequestDTO;
-import com.compassuol.sp.challenge.msuser.web.dto.UserRequestDTO;
-import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDTO;
+import com.compassuol.sp.challenge.msuser.web.dto.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User", description = "API User - Tem a responsabilidade de armazenar e gerenciar os dados de usuário.")
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService service;
 
+    @Operation(summary = "Criar um novo usuário", description = "Recurso para criar um novo usuário",
+            security = @SecurityRequirement(name = "Bearer Token Authentication"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Usuário cadastrado com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Recurso não processado devido a requisição inválida.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "409", description = "Já existe um usuário cadastrado com esse e-mail ou CPF.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    )
+            }
+    )
     @PostMapping
-    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO request) {
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserCreateRequestDTO request) {
         final UserResponseDTO response = service.createUser(request).toDTO();
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
     }
 
+    @Operation(summary = "Recuperar informações de um usuário existente",
+            description = "Recurso para recuperar um usuário existente através do Id.",
+            security = @SecurityRequirement(name = "Bearer Token Authentication"),
+            parameters = {
+                    @Parameter(name = "id", description = "Identificador (Id) do usuário no banco de dados.",
+                            in = ParameterIn.PATH, required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário recuperado com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Usuário não autenticado para acessar esse recurso.", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Usuário informado não encontrado.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> findUserById(@PathVariable("id") Long id) {
         final UserResponseDTO response = service.findUserById(id).toDTO();
@@ -32,16 +70,54 @@ public class UserController {
                 .body(response);
     }
 
+    @Operation(summary = "Atualizar um usuário existente",
+            description = "Recurso para atualizar as informações de um usuário existente através do Id.",
+            security = @SecurityRequirement(name = "Bearer Token Authentication"),
+            parameters = {
+                    @Parameter(name = "id", description = "Identificador (Id) do usuário no banco de dados.",
+                            in = ParameterIn.PATH, required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Recurso não processado devido a requisição inválida.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Usuário não autenticado para acessar esse recurso.", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Usuário informado não encontrado.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    )
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<UserResponseDTO> updateUser(
             @PathVariable("id") Long id,
-            @Valid @RequestBody UserRequestDTO request) {
+            @Valid @RequestBody UserUpdateRequestDTO request) {
         final UserResponseDTO response = service.updateUser(id, request).toDTO();
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
     }
 
+    @Operation(summary = "Atualizar a senha de um usuário existente",
+            description = "Recurso para atualizar a senha de um usuário existente através do Id.",
+            security = @SecurityRequirement(name = "Bearer Token Authentication"),
+            parameters = {
+                    @Parameter(name = "id", description = "Identificador (Id) do usuário no banco de dados.",
+                            in = ParameterIn.PATH, required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Senha do usuário alterada com sucesso.", content = @Content),
+                    @ApiResponse(responseCode = "400", description = "Recurso não processado devido a requisição inválida.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Usuário não autenticado para acessar esse recurso.", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Usuário informado não encontrado.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessageDTO.class))
+                    )
+            }
+    )
     @PutMapping("/{id}/password")
     public ResponseEntity<Void> updateUserPassword(
             @PathVariable("id") Long id,

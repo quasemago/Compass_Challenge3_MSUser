9d5ceb78eaea7d0d13c6ac9f3493258b48f9f1fa
quasemago <brunoronningfn@gmail.com>
2024-02-21 22:31:12 -0400

    feat: Implementado a classe de serviço da API da entidade 'User'.

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/service/UserService.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/service/UserService.java
@@ -1,11 +1,69 @@
 package com.compassuol.sp.challenge.msuser.domain.service;
 
+import com.compassuol.sp.challenge.msuser.domain.exception.UserDataIntegrityViolationException;
+import com.compassuol.sp.challenge.msuser.domain.exception.UserEntityNotFoundException;
+import com.compassuol.sp.challenge.msuser.domain.model.User;
 import com.compassuol.sp.challenge.msuser.domain.repository.UserRepository;
+import com.compassuol.sp.challenge.msuser.web.dto.UserRequestDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User createUser(UserRequestDTO request) {
+        try {
+            final User user = request.toModel();
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+            return repository.save(user);
+        } catch (DataIntegrityViolationException ex) {
+            throw new UserDataIntegrityViolationException("Já existe um usuário com o e-mail ou CPF informado.");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public User findUserById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new UserEntityNotFoundException("Usuário informado com o id {" + id + "} não encontrado."));
+    }
+
+    @Transactional()
+    public User updateUser(Long id, UserRequestDTO request) {
+        final User user = findUserById(id);
+
+        if (user.getEmail().equals(request.getEmail())) {
+            throw new UserDataIntegrityViolationException("Seu e-email atual é o mesmo que você está tentando atualizar.");
+        }
+        if (user.getCpf().equals(request.getCpf())) {
+            throw new UserDataIntegrityViolationException("Seu CPF atual é o mesmo que você está tentando atualizar.");
+        }
+
+        try {
+            user.setFirstName(request.getFirstName());
+            user.setLastName(request.getLastName());
+            user.setCpf(request.getCpf());
+            user.setBirthDate(request.getBirthDate());
+            user.setEmail(request.getEmail());
+            user.setCep(request.getCep());
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+            user.setActive(request.getActive());
+            return repository.saveAndFlush(user);
+        } catch (DataIntegrityViolationException ex) {
+            throw new UserDataIntegrityViolationException("Já existe um usuário com o e-mail ou CPF informado.");
+        }
+    }
+
+    @Transactional
+    public void updateUserPassword(Long id, String password) {
+        final User user = findUserById(id);
+        user.setPassword(passwordEncoder.encode(password));
+        repository.save(user);
+    }
 }

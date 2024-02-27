f088f97777de318a19ed1cb9a070960825df36c4
quasemago <brunoronningfn@gmail.com>
2024-02-27 12:04:15 -0400

    feat: Adicionado o header de autorização na requisição do endereço para autenticação.

--- src/main/java/com/compassuol/sp/challenge/msuser/infra/openfeign/client/AddressClientConsumer.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/infra/openfeign/client/AddressClientConsumer.java
@@ -2,11 +2,15 @@ package com.compassuol.sp.challenge.msuser.infra.openfeign.client;
 
 import com.compassuol.sp.challenge.msuser.web.dto.AddressResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "address-consumer", url = "http://localhost:8081/v1/address")
+
+@FeignClient(name = "address-consumer", url = "${msaddress.api-url}")
 public interface AddressClientConsumer {
     @GetMapping("/{cep}")
-    AddressResponseDTO getAddressByCep(@PathVariable("cep") String cep);
+    AddressResponseDTO getAddressByCep(@PathVariable("cep") String cep,
+                                       @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
 }
